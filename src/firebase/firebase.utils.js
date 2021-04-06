65296f502cfff7be4e9fd1f1a89997038c528413
Akash Yadav <63455785+crazylazycode@users.noreply.github.com>
2021-04-06 18:54:37 +0530

    changes my conifg to empty

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyARM-K54hnWo6p3E-WeFcSj8ue-zj8kifY",
-  authDomain: "crwn-db-67240.firebaseapp.com",
-  databaseURL: "https://crwn-db-67240.firebaseio.com",
-  projectId: "crwn-db-67240",
-  storageBucket: "crwn-db-67240.appspot.com",
-  messagingSenderId: "981879953801",
-  appId: "1:981879953801:web:b420394609c08069636958",
-  measurementId: "G-HZHPXWGBCZ"
+  // your config
 };
 
 firebase.initializeApp(config);
